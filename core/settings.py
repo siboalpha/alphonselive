b205e225977e3a3a898465fcd56ed128fb9ddbe4
siboalpha <sibo.alphonsee@gmail.com>
2022-05-21 18:25:08 +0200

    Testing .env

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("MY-SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("MY-DEBUG")
 
 ALLOWED_HOSTS = []
 
