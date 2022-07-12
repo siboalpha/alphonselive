02e25ea462f28fc5a66ad9833f6919793764204c
siboalpha <sibo.alphonsee@gmail.com>
2022-07-12 11:31:32 +0200

    third deploy

--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 #load_dotenv()
-load_dotenv("/home/admin/urm/core/.env")
+load_dotenv("/home/admin/alphonselive/core/.env")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
