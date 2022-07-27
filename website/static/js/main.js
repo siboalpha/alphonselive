67e0ff8bae4c32e03ceb5aa4581bd209a0066b9f
siboalpha <sibo.alphonsee@gmail.com>
2022-07-27 10:05:32 +0200

    Added animations

--- website/static/js/main.js
+++ website/static/js/main.js
@@ -33,3 +33,21 @@ jQuery(function($){
         }
     });
 });
+
+//Animation
+
+const items = document.querySelectorAll('.appear');
+
+const active = function(entries){
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+        entry.target.classList.add('inview'); 
+        }else{
+            entry.target.classList.remove('inview'); 
+        }
+    });
+}
+const io2 = new IntersectionObserver(active);
+ for(let i=0; i < items.length; i++){
+    io2.observe(items[i]);
+ }
