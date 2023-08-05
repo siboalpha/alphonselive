efc51bab43ea858824f4c160b51636666af918f0
SIBOMANA Alphonse <100152198+siboalpha@users.noreply.github.com>
2023-08-05 21:10:23 +0200

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("MY-SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['siboalphonse.com', 'www.siboalphonse.com', '127.0.0.1']
 
 #   EMAIL BACKEND
